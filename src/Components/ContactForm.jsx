9a3fd8d75d07486b6249dbf7f0b20c0779025b2e
Mofeleng <mofelengmokgope@gmail.com>
2023-12-28 20:00:10 +0200

    GraphQL post now working Users can now submit forms and the data is saved in the hygraph cms

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,13 +1,39 @@
 import React, { useState } from 'react'
-
+import { gql, GraphQLClient } from 'graphql-request';
 function ContactForm() {
+    const API_ENDPOINT = import.meta.env.VITE_HYGRAPH_CONTENT_API_ENDPOINT;
 
     const [ name, setName ] = useState("");
     const [ email, setEmail ] = useState("");
     const [ message, setMessage ] = useState("");
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
+        
+        try {
+            const graphQLClient = new GraphQLClient(API_ENDPOINT)
+
+            const sendMessageQuery = gql`
+                mutation sendMessage($name: String!, $email: String!, $message: String!) {
+                    createContactFormResponse(data: { name: $name, email: $email, message: $message }) {
+                        name
+                        email
+                        message
+                    }
+                }
+            `;
+
+            const variables = {
+                name,
+                email,
+                message
+            }
+            const response = await graphQLClient.request(sendMessageQuery, variables);
+            const result = await response;
+            console.log(result);
+        } catch (error) {
+            console.log("something went wrong: ", error)
+        }
         
     }
 
