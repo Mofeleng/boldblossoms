b73d2f0ce3be16cc8738275dd09d2935f132059e
Mofeleng <mofelengmokgope@gmail.com>
2023-12-28 23:44:41 +0200

    Redid the navbar and added hamburger for small screens (767px and smaller)

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -1,30 +1,41 @@
-import React from 'react'
+// Navbar.js
 
-function NavigationBar() {
+import React, { useState } from 'react';
+import './Navigationbar.css';
+
+const NavigationBar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const handleMobileMenuToggle = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <div className="navigation_bar">
-           <div className="container">
-            <div className="navbar">
-                <div className="brand">
-                    <h1 className='logo'>Boldblossoms<span className='text_primary'>.</span></h1>
-                </div>
-                <nav>
-                  <ul>
-                    <a href="/about"><li>About us</li></a>
-                    <a href="/contestants"><li>Contestents</li></a>
-                    <a href="/events"><li>Events</li></a>
-                    <a href="/blogs"><li>Blog</li></a>
-                    <a href="/results"><li>Results</li></a>
-                  </ul>
-                </nav>
-                <div className="buttons">
-                  <a href="/apply" className='btn btn_primary'>Apply now</a>
-                  <a href='/login' className='btn btn_accent'>Log in</a>
-                </div>
-            </div>
-           </div>
-        </div>
-  )
-}
+      <div className="container">
+      <nav className="navbar">
+      <div className="logo">
+        <h2 className="heading_smaller">Boldblossoms<span className="text_primary">.</span></h2>
+      </div>
+      <ul className={`nav-links ${isMobileMenuOpen ? 'mobile-menu-open' : ''}`}>
+        <li><a href="/about">About</a></li>
+        <li><a href="/partners">Partners</a></li>
+        <li><a href="/events">Events</a></li>
+        <li><a href="/contestanrs">Contestants</a></li>
+        <li><a href="/results">Results</a></li>
+        <li><a href="/apply" className={`${!isMobileMenuOpen ? 'btn btn_primary no_margin_right': ''}`}>Apply now</a></li>
+        <li><a href="/login" className={`${!isMobileMenuOpen ? 'btn btn_accent no_margin_right': ''}`}>Log in</a></li>
+      </ul>
+      <div className="hamburger-menu" onClick={handleMobileMenuToggle}>
+        <div className={`bar ${isMobileMenuOpen ? 'open' : ''}`}></div>
+        <div className={`bar ${isMobileMenuOpen ? 'open' : ''}`}></div>
+        <div className={`bar ${isMobileMenuOpen ? 'open' : ''}`}></div>
+      </div>
+    </nav>
+      </div>
+    </div>
+    
+  );
+};
 
 export default NavigationBar;
