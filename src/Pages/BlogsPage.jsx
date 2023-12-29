222f758c102587bf5c1e9efc7092d5563e51c331
Mofeleng <mofelengmokgope@gmail.com>
2023-12-29 13:57:57 +0200

    Created a blogs page Set up the route to the blog page in the main.jsx file

---
+++ src/Pages/BlogsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BlogsPage() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default BlogsPage
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import HomePage from './Pages/HomePage'
 import RegisterPage from './Pages/RegisterPage'
 import LoginPage from './Pages/LoginPage'
 import Layout from './Layout'
+import BlogsPage from './Pages/BlogsPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -26,6 +27,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path='/login' element={
           <Layout>
             <LoginPage />
+          </Layout>
+        } />
+        <Route path='/blogs' element={
+          <Layout>
+            <BlogsPage />
           </Layout>
         } />
     </Routes>
