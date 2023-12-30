a162df6df942f3fd40627aa1c7033e83f47d2a3d
Mofeleng <mofelengmokgope@gmail.com>
2023-12-30 20:52:59 +0200

    Redid the footer from scratch

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,31 +3,31 @@ import React from 'react'
 function Footer() {
   return (
     <footer className="footer">
-        <div className="container text_centered">
-            <div className="footer-content">
-                <div className="footer-section">
-                <h2 className='heading_smaller'>Contact Us</h2>
-                <p>Email: info@example.com</p>
-                <p>Phone: +1 123 456 7890</p>
-                <p></p>
-                </div>
-                <div className="footer-section">
-                <h2 className='heading_smaller'>Follow Us</h2>
-                <p>Facebook</p>
-                <p>Twitter</p>
-                <p>Instagram</p>
-                </div>
-                <div className="footer-section">
-                <h2 className='heading_smaller'>Company</h2>
-                <p>About Us</p>
-                <p>Terms of Service</p>
-                <p>Privacy Policy</p>
-                </div>
-            </div>
-            <div className="footer-bottom">
-                <p>&copy; 2023 Bold Blossoms. All rights reserved.</p>
-            </div>
+    <div className="container">
+      <div className="footer-content text_centered">
+        <div className="footer-section">
+          <h2 className='heading_smaller'>Contact Us</h2>
+          <p>Email: info@example.com</p>
+          <p>Phone: +1 123 456 7890</p>
+          <p>Phone</p>
         </div>
+        <div className="footer-section">
+          <h2 className='heading_smaller'>Follow Us</h2>
+          <p>Facebook</p>
+          <p>Twitter</p>
+          <p>Instagram</p>
+        </div>
+        <div className="footer-section">
+          <h2 className='heading_smaller'>Company</h2>
+          <p>About Us</p>
+          <p>Terms of Service</p>
+          <p>Privacy Policy</p>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>&copy; 2023 Bold Blossoms. All rights reserved.</p>
+      </div>
+    </div>
   </footer>
   )
 }
