973236a49a5bea6c3fac1268549a6b067e6d64a8
Mofeleng <mofelengmokgope@gmail.com>
2023-12-31 13:25:14 +0200

    Added basic form validation functions

--- src/Components/Pageantry/PageantInformationSection.jsx
+++ src/Components/Pageantry/PageantInformationSection.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import useEmailValidation from '../../Hooks/useEmailValidation'
 
 function PageantInformationSection({ blog_content, pageantName }) {
+
+    const [ name, setName ] = useState('');
+    const [ email, setEmail ] = useState('');
+    const [ phoneNumber, setPhoneNumber ] = useState('');
+    const [ age, setAge ] = useState('');
+    const [pageant, setPageant] = useState(pageantName);
+
+    const [ isSuccessful, setIsSuccessful ] = useState(null);
+    const [nameErr, setNameErr] = useState(null);
+    const [emailErr, setEmailErr] = useState(null);
+    const [phoneErr, setPhoneErr ] = useState(null);
+    const [ageErr, setAgeErr] = useState(null);
+
     if (!blog_content || !blog_content.html) {
         return null;
     }
-    console.log(blog_content);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!name) {
+            return setNameErr("Please fill in your name");
+        }
+        if (!email) {
+            return setEmailErr("Please fill in your email");
+        }
+        if (!phoneNumber) {
+            return setPhoneErr("Please fill in your phone number");
+        }
+        if (!age) {
+            return setAgeErr("Please fill in your age");
+        }
+
+        if (!useEmailValidation(email)) {
+            return setEmailErr("Please fill in a valid email");
+        }
+        
+    }
+    
   return (
     <section id="info">
         <div className="container">
@@ -15,13 +50,40 @@ function PageantInformationSection({ blog_content, pageantName }) {
                 <h4>Apply now</h4>
                 <div className="form">
                     <form className='apply_form'>
-                        
-                        <input type="text"  name='name' placeholder='enter your full name'/>
-                        <input type="email" name="email" placeholder='enter your email' />
-                        <input type="number" name="phoneNumber" placeholder='enter your phone number' />
+                        <input type="text"  name='name' placeholder='enter your full name'
+                            value={name}
+                            onChange={(e) => {
+                                setName(e.target.value);
+                                setNameErr(null);
+                            }}
+                        />
+                        <span className="text_red">{nameErr}</span>
+                        <input type="email" name="email" placeholder='enter your email'
+                            value={email}
+                            onChange={(e) => {
+                                setEmail(e.target.value);
+                                setEmailErr(null);
+                            }}
+                        />
+                        <span className="text_red">{emailErr}</span>
+                        <input type="number" name="phoneNumber" placeholder='enter your phone number'
+                            value={phoneNumber}
+                            onChange={(e) => {
+                                setPhoneNumber(e.target.value);
+                                setPhoneErr(null);
+                            }}
+                        />
+                        <span className="text_red">{phoneErr}</span>
                         <input type="text" name='pageant' value={pageantName} />
-                        <input type="number" name="age" placeholder='how old are you?' />
-                        <button className='btn btn_primary btn_lg'>Submit</button>
+                        <input type="number" name="age" placeholder='how old are you?'
+                            value={age}
+                            onChange={(e) => {
+                                setAge(e.target.value);
+                                setAgeErr(null);
+                            }}
+                        />
+                        <span className="text_red">{ageErr}</span>
+                        <button className='btn btn_primary btn_lg' onClick={(e) => handleSubmit(e)}>Submit</button>
 
                     </form>
                     <div className="more_info">
