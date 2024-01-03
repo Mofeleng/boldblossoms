46194d298ef0c8dcc1247e901d840f605e121039
Mofeleng <mofelengmokgope@gmail.com>
2024-01-03 19:51:48 +0200

    Replaced a tags with Link tags in the Hero section of the home page

--- src/Components/Home/HeroSection.jsx
+++ src/Components/Home/HeroSection.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function HeroSection() {
   return (
@@ -10,8 +11,8 @@ function HeroSection() {
             Step into a world where boldness meets blossoming beauty. Welcome to the "Bold Blossoms" pageant, where we celebrate the strength, resilience, and grace that define empowered elegance. Immerse yourself in a tapestry of vibrant magenta hues symbolizing energy and passion, set against a canvas of pure white that echoes the simplicity of strength. As our contestants embrace the stage, a subtle touch of gold accentuates their individuality and sophistication..
             </p>
             <div className="buttons_hero">
-              <a href="/apply/miss-bold-blossoms-24" className='btn btn_primary'>Apply now</a>
-              <a href="/partners" className='btn btn_accent text_white'>Become a sponsor</a>
+              <Link to="/apply/miss-bold-blossoms-24" className='btn btn_primary'>Apply now</Link>
+              <Link to="/partners" className='btn btn_accent text_white'>Become a sponsor</Link>
             </div>
           </div>
           </div>
