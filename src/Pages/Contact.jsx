51a30a179c8f55453942f324e71e24aa64880b55
Mofeleng <mofelengmokgope@gmail.com>
2024-01-05 21:48:19 +0200

    Added content to the contact page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import SolidBGHeroTemplate from '../Components/UserInterface/SolidBGHeroTemplate'
+import ContactForm from '../Components/ContactForm'
 
 function Contact() {
   return (
     <>
-    
+        <SolidBGHeroTemplate
+            label={"Contact us"}
+            heading={"fill out our contact form and we’ll be in touch"}
+            paragraph={`If you have any queries you want answered feel free to send us an email and we’ll be in  touch.`}
+        />
+
+        <section id="contact_form">
+            <div className="container">
+                <h4 className="heading_smaller">Contact us</h4>
+                <ContactForm />
+            </div>
+        </section>
     </>
   )
 }
