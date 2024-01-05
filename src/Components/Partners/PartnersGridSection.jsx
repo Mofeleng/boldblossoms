d8a149319dc9e9bdad331a3938b5750c2724f9c5
Mofeleng <mofelengmokgope@gmail.com>
2024-01-05 21:16:55 +0200

    Added current sponsors and styled grid on desktop

---
+++ src/Components/Partners/PartnersGridSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function PartnersGridSection() {
+  return (
+    <section id="partners">
+        <div className="container">
+            <div className="partners">
+                <div className="partner">
+                    <img src="/src/assets/mundane_logo.jpg" alt="" />
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default PartnersGridSection
--- src/Pages/Partners.jsx
+++ src/Pages/Partners.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import SOlidBGHeroTemplate from '../Components/UserInterface/SolidBGHeroTemplate'
+import SolidBGHeroTemplate from '../Components/UserInterface/SolidBGHeroTemplate'
+import PartnersGridSection from '../Components/Partners/PartnersGridSection'
 
 function Partners() {
   return (
     <>
-    
+        <SolidBGHeroTemplate
+            label={"Our partners"}
+            heading={"All the people making this all possible"}
+            paragraph={`Our partners and sponsors are the reason we are able to empower the bold blossoms everywhere. `}
+        />
+        <PartnersGridSection />
     </>
   )
 }
